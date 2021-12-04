a6258a3b4759b59e03deb491a82449d610294bab
PRITISH RANA <ishucool567@gmail.com>
2021-12-04 19:16:03 +0530

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var counterPlaceHolder = document.getElementById("counter-placeholder");
+var btnIncrement = document.getElementById("btn-increment");
+var btnDecrement = document.getElementById("btn-decrement");
+
+var number = 0;
+function changeColor(number){
+    var color = "";
+    if(number < 0 ){
+        color = "red";
+    }else if (number > 0 ){
+        color = "green";
+    }else{
+        color="white";
+    }
+    return color;
+}
+
+btnIncrement.addEventListener("click", function(){
+    number++;
+    counterPlaceHolder.innerHTML = number;
+    counterPlaceHolder.style.color = changeColor(number);
+});
+
+btnDecrement.addEventListener("click", function(){
+    number--;
+    counterPlaceHolder.innerHTML = number;
+    counterPlaceHolder.style.color = changeColor(number);
+});
